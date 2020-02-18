1021b17e7ca27dea530d3a7b1ec6db7fcd4c0a31
Mariam Anuashvili <m.anuashvili@gmail.com>
2020-02-18 23:06:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,21 +29,5 @@ function removeLastDriver(){
 }
 
 function removeFirstDriver(){
-  
+   return drivers.slice(0);
 }
-
-    describe('removeLastDriver()', function () {
-      it('removes the last driver in the drivers array and returns a new array, leaving the drivers array unchanged', function () {
-        expect(removeLastDriver()).to.have.ordered.members(["Milo", "Otis"]);
-
-        expect(drivers).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
-      });
-    });
-
-    describe('removeFirstDriver()', function () {
-      it('removes the first driver from the drivers array and returns a new array, leaving the drivers array unchanged', function () {
-        expect(removeFirstDriver()).to.have.ordered.members(["Otis", "Garfield"]);
-
-        expect(drivers).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
-      });
- 
