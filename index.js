fbb3bd8ba56e37490dbd7c54b3ae4a53bfa249f6
Mariam Anuashvili <m.anuashvili@gmail.com>
2020-02-18 22:49:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
 let drivers = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendDriver(name){
-  return drivers.push(name);
+  drivers.push(name);
 }
-
-describe('destructivelyAppendDriver(name)', function () {
-      it('appends a driver to the end of the drivers array', function () {
-        destructivelyAppendDriver('Ralph');
-
-        expect(drivers).to.have.ordered.members(["Milo", "Otis", "Garfield", "Ralph"]);
-      });
-    });
