78e238c288c0c38a77b8d79bec10235730cfe160
Mariam Anuashvili <m.anuashvili@gmail.com>
2020-02-18 22:45:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Write your solution here!
+function drivers(){
+  
+}
+
+describe('index.js', function () {
+  describe('drivers', function () {
+    it('is assigned an initial value of ["Milo", "Otis", "Garfield"]', function () {
+      expect(drivers).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
+    });
+  });
+
+  describe('Array functions', function () {
+    beforeEach(function () {
+      drivers.length = 0;
+
+      drivers.push('Milo', 'Otis', 'Garfield');
+    });
