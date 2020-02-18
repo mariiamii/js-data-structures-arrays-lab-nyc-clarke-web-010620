d8457d8132db04d3e79d990049280c8dc2b0cae2
Mariam Anuashvili <m.anuashvili@gmail.com>
2020-02-18 22:54:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,55 @@ function destructivelyRemoveLastDriver(){
   drivers.pop();
 }
 
+function destructivelyRemoveFirstDriver(){
+  drivers.shift();
+}
+
+function appendDriver(name){
+  
+}
+
+function prependDriver(name){
+  
+}
+
+function removeLastDriver(){
+  
+}
+
+function removeFirstDriver(){
+  
+}
+
+
+   describe('appendDriver(name)', function () {
+      it('appends a driver to the drivers array and returns a new array, leaving the drivers array unchanged', function () {
+        expect(appendDriver("Broom")).to.have.ordered.members(["Milo", "Otis", "Garfield", "Broom"]);
+
+        expect(drivers).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
+      });
+    });
+
+    describe('prependDriver(name)', function () {
+      it('prepends a driver to the drivers array and returns a new array, leaving the drivers array unchanged', function () {
+        expect(prependDriver("Arnold")).to.have.ordered.members(["Arnold", "Milo", "Otis", "Garfield"]);
+
+        expect(drivers).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
+      });
+    });
+
+    describe('removeLastDriver()', function () {
+      it('removes the last driver in the drivers array and returns a new array, leaving the drivers array unchanged', function () {
+        expect(removeLastDriver()).to.have.ordered.members(["Milo", "Otis"]);
+
+        expect(drivers).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
+      });
+    });
+
+    describe('removeFirstDriver()', function () {
+      it('removes the first driver from the drivers array and returns a new array, leaving the drivers array unchanged', function () {
+        expect(removeFirstDriver()).to.have.ordered.members(["Otis", "Garfield"]);
+
+        expect(drivers).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
+      });
+ 
