26c9e404c6bd16766cab1459fc2ac461dbe092a4
Mariam Anuashvili <m.anuashvili@gmail.com>
2020-02-18 22:49:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 let drivers = ["Milo", "Otis", "Garfield"];
 
-describe('index.js', function () {
-  describe('drivers', function () {
-    it('is assigned an initial value of ["Milo", "Otis", "Garfield"]', function () {
-      expect(drivers).to.have.ordered.members(["Milo", "Otis", "Garfield"]);
-    });
-  });
+function destructivelyAppendDriver(name){
+  return drivers.push(name);
+}
 
-  describe('Array functions', function () {
-    beforeEach(function () {
-      drivers.length = 0;
+describe('destructivelyAppendDriver(name)', function () {
+      it('appends a driver to the end of the drivers array', function () {
+        destructivelyAppendDriver('Ralph');
 
-      drivers.push('Milo', 'Otis', 'Garfield');
+        expect(drivers).to.have.ordered.members(["Milo", "Otis", "Garfield", "Ralph"]);
+      });
     });
